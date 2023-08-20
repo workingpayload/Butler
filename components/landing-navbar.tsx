73fd4ccfb6c4b4e40509ae718c7a59aa787fa397
Raj <rs91963@gmail.com>
2023-08-20 19:02:55 +0530

    another commit

--- components/landing-navbar.tsx
+++ components/landing-navbar.tsx
@@ -18,17 +18,18 @@ export const LandingNavbar = () => {
     <nav className="p-4 bg-transparent flex items=center justify-between">
       <Link href="/" className="flex items-center">
         <div className="relative h-8 w-8 mr-4">
-          <Image fill alt="Logo" src="/logo.png" />
+          <Image fill alt="Logo" src="/Logo.png" />
         </div>
         <h1 className={cn("text-2xl font-bold text-white", font.className)}>
           Butler
         </h1>
       </Link>
       <div className="flex items-center gap-x-2">
-            <Link href={isSignedIn ? "/dashboard" : "/sign-up"}/>
+            <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
             <Button variant={"outline"} className="rounded-full">
                 Get Started!
             </Button>
+            </Link>
       </div>
     </nav>
   );
